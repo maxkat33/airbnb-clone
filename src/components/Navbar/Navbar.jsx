5425ad3b0206eccfe52ecfbc00bb58d8b628b0be
maxkat33 <max.katilius@gmail.com>
2023-05-06 12:18:43 +1000

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AirbnbLogo from '../../images/airbnb-logo.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        Navbar
+        <img src={ AirbnbLogo } alt='Airbnb Logo'/>
+    </nav>
+  )
+}
+
+export default Navbar
